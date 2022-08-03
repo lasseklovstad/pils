abba9d36c6201ce4df3047aa41568bd19e34faca
Lasse Kløvstad <lasse.klovstad@ciber.no>
2022-08-03 22:21:19 +0200

    Legg til vite frontend

---
+++ pils-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div>
+      <header className={"bg-amber-100 flex justify-center align-middle p-2"}>
+          <h1 className="text-4xl text-black font-medium">Pils</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
