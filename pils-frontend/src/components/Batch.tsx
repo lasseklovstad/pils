47daa470f085541d9605cf7ecd347d4b6613e917
Lasse Kløvstad <lasse.klovstad@gmail.com>
2022-08-07 22:31:00 +0200

    Remove dot

--- pils-frontend/src/components/Batch.tsx
+++ pils-frontend/src/components/Batch.tsx
@@ -27,7 +27,7 @@ export const Batch = ({batch}: BatchProps) => {
                     }} />
                     <YAxis allowDecimals domain={['dataMin', 'dataMax']}  />
                     <Tooltip />
-                    <Line type="monotone" dataKey="temp" stroke="#82ca9d" strokeWidth={3} />
+                    <Line type="monotone" dataKey="temp" stroke="#82ca9d" strokeWidth={3} dot={false} />
                 </LineChart>
             </ResponsiveContainer>
             <div>
